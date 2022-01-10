c28f4755d24a7d00eb1f97c39773a368a550e285
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2022-01-09 21:46:59 -0300

    Empezando Tetris

---
+++ Tetris/juego.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
+int main(){
+    //CREANDO VENTANA DEL JUEGO
+
+    RenderWindow window(VideoMode(320, 480), "Tetris");
+
+    while (window.isOpen()){
+        
+    }
+}
